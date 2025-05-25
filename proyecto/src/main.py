82229f588f968dc205233065e350488d5139e661
Paul Lora <bryanr_122@hotmail.com>
2025-05-24 20:28:49 -0500

    setup project

---
+++ proyecto/src/main.py
@@ -0,0 +1,23 @@
+
+from src import __version__
+from src.api import API
+
+app = API(
+    openapi_url="/openapi.json",
+    title="Proyecto IR - 2025A",
+    version=__version__.__version__,
+    responses={
+        401: {"description": "Unauthorized - Missing or Invalid Token"},
+        403: {"description": "Forbidden - Insufficient Permissions"},
+        404: {"description": "Not Found - Resource Not Found"},
+        400: {"description": "Bad Request - Validation Errors"},
+    },
+    docs_url=None,
+    redoc_url="/docs",
+)
+
+
+app.include_router(actuator.router)
+app.add_router(teams.router)
+
+customize_openapi(app)
